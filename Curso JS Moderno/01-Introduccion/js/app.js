3a72e099618a60ee73bb4d015ebccb5d8d205610
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-12-21 13:25:23 +0100

    Final capitulo 01

---
+++ Curso JS Moderno/01-Introduccion/js/app.js
@@ -0,0 +1,11 @@
+//const nombre = prompt("Cual es tu nombre:");
+//document.querySelector(".contenido").innerHTML = `${nombre}`;
+
+console.log("Hola");
+console.log("Mundo");
+
+function hola() {
+  console.log("ok");
+  console.log("Hola");
+  console.log("Mundo");
+}
