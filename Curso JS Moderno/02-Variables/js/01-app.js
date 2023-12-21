b67bb2dba8baeb487c7a48c3e085327890510fb9
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-12-21 13:46:25 +0100

    Variables 01 app

---
+++ Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,27 @@
+// Se puede inicializar sin valor y asignarlos despues
+var disponible;
+
+disponible = true;
+
+disponible = flase;
+
+// Inicializar multiples variables
+
+var categoria = "laptop",
+  marca = "Oki",
+  calificacion = 5;
+
+//  Las variables no se pieden iniciar con numeros
+// var 99dias;
+var dias99;
+
+// var 01__;
+var _01;
+var _usuario;
+var __usuarios;
+
+// CamelCase
+var nombreProducto;
+
+// snike
+var nombre_producto;
