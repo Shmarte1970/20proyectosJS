f0f27193b9880cef7e7bb08c29bc3eaa95b802c2
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-12-21 14:02:28 +0100

    03 Variables Const

---
+++ Curso JS Moderno/02-Variables/js/03-app.js
@@ -0,0 +1,8 @@
+// Las variables con const se tienen que inicializar
+
+const product = "Table";
+
+// Las variables inicializadas con cons no se pueden reasignar
+// product = "Monitor";
+
+console.log(product);
