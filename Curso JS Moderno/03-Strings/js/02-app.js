9622bf72ed6b5bea7a59fc84d9c3d4ad2bb653be
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2023-12-21 14:48:42 +0100

    String 02

---
+++ Curso JS Moderno/03-Strings/js/02-app.js
@@ -0,0 +1,17 @@
+// Metodos de los String
+
+const producto02 = "la marca del coche es Citroën";
+
+console.log(producto02);
+
+// Conocer la longitud de la cadena de textos
+console.log(producto02.length);
+
+// Conocer la posicion en la que empieza algo dentro del string
+console.log(producto02.indexOf("coche"));
+
+// Conover si dentro de una cadena string existe o no algo
+
+console.log(producto02.includes("avion")); // por consola false no existe
+
+console.log(producto02.includes("coche")); // por consola true si existe
