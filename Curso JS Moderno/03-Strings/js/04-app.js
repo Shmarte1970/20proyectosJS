e3bc747bb06af9281d78690d379e3760aebbbc22
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2024-01-04 14:38:06 +0100

    Push 4 de Enero de 2024

---
+++ Curso JS Moderno/03-Strings/js/04-app.js
@@ -0,0 +1,10 @@
+const producto = "         Monitor 20 Pulgadas    .      ";
+
+console.log(producto);
+console.log(producto.length);
+
+console.log(producto.trimStart());
+
+console.log(producto.trimEnd());
+
+console.log(producto.trimStart().trimEnd().length); //);
