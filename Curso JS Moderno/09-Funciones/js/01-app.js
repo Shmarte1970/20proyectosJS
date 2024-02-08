afce5d509382caff1b19d3b4fdec7bcc98786c5f
programacionpc\Pedro Ríos <riosmacias@gmail.com>
2024-02-08 12:02:44 +0100

    update 20 projectos

---
+++ Curso JS Moderno/09-Funciones/js/01-app.js
@@ -0,0 +1,11 @@
+function sumar() {
+  console.log(2 + 2);
+}
+
+sumar();
+
+const sumar2 = function (val1, val2) {
+  console.log(val1 + val2);
+};
+
+sumar2(5, 7);
---
+++ Curso JS Moderno/09-Funciones/js/02-app.js
@@ -0,0 +1,9 @@
+sumar();
+function sumar() {
+  console.log(2 + 2);
+}
+
+sumar2(5, 7);
+const sumar2 = function (val1, val2) {
+  console.log(val1 + val2);
+};
